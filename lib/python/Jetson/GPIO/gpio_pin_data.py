NVIDIA/jetson-gpio

commit 01bd342b6ea7b823ecd42532fca8c09d6bc5d5a7

Warn if running on an unsupported carrier board
It is likely that any carrier board other than the developer kit carrier
board uses a different set of GPIOs for any GPIO expansion connector that
may be present. Thus, it is unlikely the code will operate correctly with
any non-default carrier board. Warn when this situation is detected, or
if the files necessary to determine this are not present. This notifies
the user of the issue, without preventing use of the library in the case
where the carrier board does happen to use the expected set of GPIOs.

lib/python/Jetson/GPIO/gpio_pin_data.py
@@ -18,6 +18,8 @@
 # DEALINGS IN THE SOFTWARE.
 
 import os
+import os.path
+import sys
 
 JETSON_XAVIER = 'JETSON_XAVIER'
 JETSON_TX2 = 'JETSON_TX2'
@@ -228,6 +230,9 @@ class ChannelInfo(object):
         self.pwm_id = pwm_id
 
 
+ids_warned = False
+
+
 def get_data():
     compatible_path = '/proc/device-tree/compatible'
     ids_path = '/proc/device-tree/chosen/plugin-manager/ids'
@@ -237,25 +242,52 @@ def get_data():
 
     def matches(vals):
         return any(v in compatibles for v in vals)
+
+    def find_pmgr_board(prefix):
+        global ids_warned
+        if not os.path.exists(ids_path):
+            if not ids_warned:
+                ids_warned = True
+                msg = """\
+WARNING: Plugin manager information missing from device tree.
+WARNING: Cannot determine whether the expected Jetson board is present.
+"""
+                sys.stderr.write(msg)
+            return None
+        for f in os.listdir(ids_path):
+            if f.startswith(prefix):
+                return f
+        return None
+
+    def warn_if_not_carrier_board(carrier_board):
+        found = find_pmgr_board(carrier_board + '-')
+        if not found:
+            msg = """\
+WARNING: Carrier board is not from a Jetson Developer Kit.
+WARNNIG: Jetson.GPIO library has not been verified with this carrier board,
+WARNING: and in fact is unlikely to work correctly.
+"""
+            sys.stderr.write(msg)
+
     if matches(compats_tx1):
         model = JETSON_TX1
+        warn_if_not_carrier_board('2597')
     elif matches(compats_tx2):
         model = JETSON_TX2
+        warn_if_not_carrier_board('2597')
     elif matches(compats_xavier):
         model = JETSON_XAVIER
+        warn_if_not_carrier_board('2822')
     elif matches(compats_nano):
         model = JETSON_NANO
-        module_id = None
-        for f in os.listdir(ids_path):
-            if f.startswith('3448-'):
-                module_id = f
-                break
+        module_id = find_pmgr_board('3448')
         if not module_id:
-            raise Exception('Could not determine Jetson Nano revision')
+            raise Exception('Could not determine Jetson Nano module revision')
         revision = f.split('-')[-1]
         # Revision is an ordered string, not a decimal integer
         if revision < "200":
-            raise Exception('Jetson Nano revision must be A02 or later')
+            raise Exception('Jetson Nano module revision must be A02 or later')
+        warn_if_not_carrier_board('3449')
     else:
         raise Exception('Could not determine Jetson model')
 

commit c6b0b6ad6b94466d2f33825319c76ed865ca8185

Fix module ID detection on Jetson Nano
Fixes: 01bd342b6ea7 ("Warn if running on an unsupported carrier board")

lib/python/Jetson/GPIO/gpio_pin_data.py
@@ -283,7 +283,7 @@ WARNING: and in fact is unlikely to work correctly.
         module_id = find_pmgr_board('3448')
         if not module_id:
             raise Exception('Could not determine Jetson Nano module revision')
-        revision = f.split('-')[-1]
+        revision = module_id.split('-')[-1]
         # Revision is an ordered string, not a decimal integer
         if revision < "200":
             raise Exception('Jetson Nano module revision must be A02 or later')