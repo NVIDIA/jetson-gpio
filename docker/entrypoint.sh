430ae6ffb896ea6951b63078b03ea119204e8a95
jueon.park <bluegbgb@gmail.com>
2022-06-01 16:01:35 +0900

    add a sample Dockerfile

---
+++ docker/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+set -e
+
+if [ -z "${USER}" ]; then
+  USER="user"
+fi
+
+# if UID, GID and GID_GPIO are specified
+if [ ! -z "${UID}" ] && [ ! -z "${GID}" ] && [ ! -z "${GID_GPIO}" ]; then
+  # create groups 
+  groupadd -f -r -g $GID_GPIO gpio
+  groupadd -f -r -g $GID $USER
+
+  # create a user
+  useradd -m --uid $UID -g $GID_GPIO $USER --groups $GID,$USER 
+
+  # execute the command as the user
+  su ${USER} --command "exec $@"
+else
+  # execute the command as root
+  exec $@
+fi
